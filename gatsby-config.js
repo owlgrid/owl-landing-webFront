6a8b44e2bc2c217882474795091dccfd8bd5b9af
Grégoire <gregoire.thibo@gmail.com>
2022-08-05 08:47:49 +0200

    remove manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,18 +39,6 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `images/logo.png`, // This path is relative to the root of the site.
-            },
-        },
-        {
             resolve: 'gatsby-plugin-eslint',
             options: {
                 test: /\.ts$|\.tsx$/,
