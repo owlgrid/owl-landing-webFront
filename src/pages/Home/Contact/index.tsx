b7b042d200d90802005d02fbfdb011e1d57146af
Grégoire <gregoire.thibo@gmail.com>
2022-08-05 10:15:38 +0200

    fix size issue

--- src/pages/Home/Contact/index.tsx
+++ src/pages/Home/Contact/index.tsx
@@ -3,29 +3,27 @@ import { Text } from '../../../components/Text';
 
 export const Contact = () => {
     return (
-        <section className="py-24 bg-white tails-selected-element px-20 mx-auto max-w-7xl" id="contact">
-            <div className="px-20 mx-auto max-w-7xl">
-                <div className="flex flex-col justify-center space-y-8 items-center">
-                    <Text style="h2" align='center'>
-                        Stay in touch!
-                    </Text>
-                    <Text style="headerLine3" align='center'>
-                        Are you interested in OwlGrid? Follow our journey on LinkedIn to stay informed of the product
-                        development!
-                    </Text>
-                    <a
-                        href="https://www.linkedin.com/company/owlgrid/"
-                        target='_blank'
-                        className="flex-shrink-0 px-10 py-5 text-2xl font-medium text-center text-white bg-blue-600 rounded-lg focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 focus:ring-offset-2 focus:outline-none"
-                        data-primary="blue-600"
-                        data-rounded="rounded-lg"
-                    >
-                        LinkedIn — OwlGrid
-                    </a>
-                    <Text style='p' customStyle='text-gray-400'>
-                        Or contact us on <a href="mailto:hello@creastel.com">hello@creastel.com</a>.
-                    </Text>
-                </div>
+        <section className="py-24 bg-white tails-selected-element px-12 mx-auto" id="contact">
+            <div className="flex flex-col justify-center space-y-8 items-center">
+                <Text style="h2" align='center'>
+                    Stay in touch!
+                </Text>
+                <Text style="headerLine3" align='center'>
+                    Are you interested in OwlGrid? Follow our journey on LinkedIn to stay informed of the product
+                    development!
+                </Text>
+                <a
+                    href="https://www.linkedin.com/company/owlgrid/"
+                    target='_blank'
+                    className="flex-shrink-0 px-10 py-5 text-2xl font-medium text-center text-white bg-blue-600 rounded-lg focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 focus:ring-offset-2 focus:outline-none"
+                    data-primary="blue-600"
+                    data-rounded="rounded-lg"
+                >
+                    LinkedIn — OwlGrid
+                </a>
+                <Text style='p' customStyle='text-gray-400'>
+                    Or contact us on <a href="mailto:hello@creastel.com">hello@creastel.com</a>.
+                </Text>
             </div>
         </section>
     );
