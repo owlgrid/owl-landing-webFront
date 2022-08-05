92e95689d65db12ff65aec167d42ead28437ac10
Grégoire <gregoire.thibo@gmail.com>
2022-08-05 10:16:14 +0200

    remove qna section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,7 +14,7 @@ export const Home = () => (
         <Philosphy />
         <Workflows />
         <Features />
-        <QnA />
+        {/* <QnA /> */}
         <Contact />
         <Footer />
     </>
