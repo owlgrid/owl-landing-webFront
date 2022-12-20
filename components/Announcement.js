33bf04ddaeb3f70e2426332a6491ad5b47279da7
Grégoire <gregoire.thibo@gmail.com>
2022-12-20 11:21:46 +0100

    add link to contact

--- components/Announcement.js
+++ components/Announcement.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Announcement(props) {
     return (
-        <Link href="/">
+        <Link href="#contact">
             <div className="primary-gradient flex justify-center items-center gap-4 py-4 text-white">
                 <BellIcon className="h-6 w-6" />
                 <p className="text-sm sm:text-center text-white">Click here to stay informed about the project</p>
